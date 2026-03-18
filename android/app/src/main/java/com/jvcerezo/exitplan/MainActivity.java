9a98c9a1f6229355e9c3ac6c0aaed237757512ea
jvcerezo <jvcerezo@up.edu.ph>
2026-03-18 12:16:33 +0800

    ayoko na

--- android/app/src/main/java/com/jvcerezo/exitplan/MainActivity.java
+++ android/app/src/main/java/com/jvcerezo/exitplan/MainActivity.java
@@ -1,22 +1,11 @@
 package com.jvcerezo.exitplan;
 
-import android.content.Intent;
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import ee.forgr.capacitor.social.login.GoogleProvider;
-import ee.forgr.capacitor.social.login.SocialLoginPlugin;
 import ee.forgr.capacitor.social.login.ModifiedMainActivityForSocialLoginPlugin;
 
 public class MainActivity extends BridgeActivity implements ModifiedMainActivityForSocialLoginPlugin {
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        SocialLoginPlugin plugin = (SocialLoginPlugin) this.bridge.getPlugin("SocialLogin").getInstance();
-        if (plugin != null) {
-            GoogleProvider googleProvider = plugin.getGoogleProvider();
-            if (googleProvider != null) {
-                googleProvider.handleOnActivityResult(requestCode, resultCode, data);
-            }
-        }
+    public void IHaveModifiedTheMainActivityForTheUseWithSocialLoginPlugin() {
+        // Required marker method for @capgo/capacitor-social-login
     }
 }
